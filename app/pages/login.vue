054e9dff58f0e32d21855afbf463500106401619
Tiburce Kouagou <kouagoutiburce@gmail.com>
2025-09-09 15:29:42 +0100

    feat: implement redirect after successful login based on query parameters

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -9,7 +9,9 @@ const { $auth } = useNuxtApp()
 const handleLogin = async () => {
   try {
     await authStore.login($auth, email.value, password.value)
-    navigateTo('/')
+    const q = useRoute().query
+    const redirectTo = (q.redirectTo as string) || '/'
+    navigateTo(redirectTo)
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   catch (error: any) {
